89238172ebd0a048f2fd2597dc8b024ba7a85dd7
Scott Davis <sdavis@scott-daviss-mac-mini.local>
2010-01-23 19:15:02 -0500

    added requires

---
+++ github.php
@@ -0,0 +1,8 @@
+<?php
+	require_once(__DIR__ . '/lib/loader.php');
+	require_once(__DIR__ . '/lib/commits.php');
+	require_once(__DIR__ . '/lib/issues.php');
+	require_once(__DIR__ . '/lib/objects.php');
+	require_once(__DIR__ . '/lib/repos.php');
+	require_once(__DIR__ . '/lib/users.php');
+
